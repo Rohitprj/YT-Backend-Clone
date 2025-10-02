2965f6c92e5bb5fc25d6f67652d5d745fea825e7
Rohit kumar <rohitprj.1282@gmail.com>
2025-10-02 17:55:35 +0530

    util error files

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,22 @@
-const asyncHandler = (fun) => async (req, res, next) => {
-  try {
-    await fun(req, res, next);
-  } catch (error) {
-    res.status(error.code || 500).json({
-      status: false,
-      message: error.message,
-    });
-  }
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
 };
+
+export { asyncHandler };
+
+// const asyncHandler = (fun) => async (req, res, next) => {
+//   try {
+//     await fun(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       status: false,
+//       message: error.message,
+//     });
+//   }
+// };
+
+// export { asyncHandler };
