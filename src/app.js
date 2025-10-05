4ee77981d44d2f03c58f44a3c560a1da339614a5
Rohit kumar <rohitprj.1282@gmail.com>
2025-10-05 20:43:33 +0530

    fix: correct cookieParser usage and improve asyncHandler function return statement

--- src/app.js
+++ src/app.js
@@ -14,16 +14,18 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-app.use(cookieParser);
+app.use(cookieParser());
 
-// imports
+// Router imports
 import userRoutes from "./routes/user.routes.js";
+
 // Routes
 app.use("/api/v1/users", userRoutes);
 
 app.use("/", (req, res) => {
   res.send({
-    message: "Server is running fine",
+    success: true,
+    message: "Server is running",
   });
 });
 
