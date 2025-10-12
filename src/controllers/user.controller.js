d646719c0ed83196be8ab592016beaf74841af49
Rohit kumar <rohitprj.1282@gmail.com>
2025-10-12 21:35:24 +0530

    work

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,10 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, fullName, password } = req.body;
+
+  console.log(email);
+
   res.status(200).json({
     message: "ok",
   });
