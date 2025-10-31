b5593608b3577e669988a53b37be7f208dd26daf
Rohit kumar <rohitprj.1282@gmail.com>
2025-10-31 16:50:15 +0530

    fix: await cloudinary upload and improve error handling in uploadOnCloudinary function

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { app } from "./app.js";
 dotenv.config({ path: "./.env" });
 
 const PORT = process.env.PORT || 8000;
-// console.log(`server is running on http://localhost:${PORT}`);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -10,12 +10,14 @@ cloudinary.config({
 const uploadOnCloudinary = async (localFilePath) => {
   try {
     if (!localFilePath) return null;
-    const response = cloudinary.uploader.upload(localFilePath, {
+    const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
     console.log("File successfully uploaded to the cloudinary", response.url);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
+    console.log("Cloudinary error");
     fs.unlinkSync(localFilePath);
     return null;
   }
